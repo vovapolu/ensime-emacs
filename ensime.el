de0826166e402e68264bc2ffef78e73394124a3c
Sam Halliday <sam.halliday@gmail.com>
2016-01-27 21:46:44 +0000

    remove a global variable

--- ensime.el
+++ ensime.el
@@ -76,21 +76,19 @@
 
 ;;;###autoload
 (defun ensime ()
-  "Read config file for settings. Then start an inferior
-   ENSIME server and connect to its Swank server."
+  "Read config file for settings then start an ensime-server and connect."
   (interactive)
-  ; We have to use setq instead of let here because of scoping
-  (setq orig-bfn buffer-file-name)
-  (condition-case ex
-      (if ensime-auto-generate-config
-          (ensime--maybe-refresh-config
-           nil
-           '(lambda () (ensime--maybe-update-and-start-noninteractive orig-bfn))
-           '(lambda (reason) (ensime--maybe-update-and-start-noninteractive orig-bfn)))
-        (ensime--maybe-update-and-start orig-bfn))
-    ('error (error (format
-                    "check that sbt is on your PATH and that your config is compatible with %s [%s]"
-                    "http://github.com/ensime/ensime-server/wiki/Example-Configuration-File" ex)))))
+  (let ((orig-bfn buffer-file-name))
+    (condition-case ex
+        (if ensime-auto-generate-config
+            (ensime--maybe-refresh-config
+             nil
+             `(lambda () (ensime--maybe-update-and-start-noninteractive ,orig-bfn))
+             `(lambda (reason) (ensime--maybe-update-and-start-noninteractive ,orig-bfn)))
+          (ensime--maybe-update-and-start orig-bfn))
+      ('error (error (format
+                      "check that sbt is on your PATH and that your config is compatible with %s [%s]"
+                      "http://github.com/ensime/ensime-server/wiki/Example-Configuration-File" ex))))))
 
 ;;;###autoload
 (defun ensime-remote (host port)
